0503ac4d4eb8d21f001e5d884e2fc57fe92e4975
ekarobyanto <robyantoeka@gmail.com>
2025-02-11 07:35:50 +0700

    init

--- db/sql/create_books.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS books (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    cover_image_url VARCHAR(255),
-    is_available BOOLEAN DEFAULT TRUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
